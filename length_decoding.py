caae8f648b8326fd336c1437f89ab721b4d04a1b
Artsiom Praneuski <artem.pronevskiy@gmail.com>
2020-09-18 15:54:36 +0300

    Create length_decoding.py

---
+++ length_decoding.py
@@ -0,0 +1,45 @@
+# Length decoding problem
+# "a3b5g2a4n6" -> "aaabbbbbggaaaannnnnn"
+# env: python3.8
+
+from re import match, compile
+
+digit_pattern = compile(r"\d")
+letter_pattern = compile(r"\D")
+
+s = "a1b2c3g1c2d3e4"
+print(s)
+
+decoded_s = ""
+current_char = None
+chars_group_len_str = None
+
+for i in range(len(s)):
+    char = s[i]
+
+    if not match(digit_pattern, char):
+        # first letter or letter from next chars group found,
+        # need to save info about previous chars group
+        if chars_group_len_str:
+            chars_group_len = int(chars_group_len_str)
+            decoded_s += current_char * chars_group_len
+
+        # reset chars_group_len_str
+        chars_group_len_str = None
+
+        # override current_char by current one
+        current_char = char
+    else:
+        # digit found
+        if not chars_group_len_str:
+            chars_group_len_str = char
+        else:
+            chars_group_len_str += char
+
+    if i == len(s) - 1:
+        # last char found
+        chars_group_len = int(chars_group_len_str)
+        decoded_s += current_char * chars_group_len
+
+
+print(decoded_s)
